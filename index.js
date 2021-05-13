31415c6cfea3346bc43371bb6819488fc8ebbfd9
hcast00 <hcast00@estudiantes.unileon.es>
2021-05-13 10:50:41 +0200

    Funcionando corregir Falta añadir 2 pasatiempos mas

--- index.js
+++ index.js
@@ -1,88 +1,115 @@
-const  express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 const application = express();
-const bodyParser = require('body-parser');
-application.use(bodyParser.json()); 
 
-application.post('/palabras', (req, res) => {
-    console.log('Got body:', req.body.palabras);
-   // res.end(checkCascada(JSON.parse(req.body)));
+application.use(bodyParser.urlencoded({ extended: true }));
+application.use(bodyParser.json());
+
+application.post("/palabras", function (req, res) {
+  console.log(req.body.palabras); // this outputs: { data: 'hello' }
+  palabras = req.body.palabras.split(',');
+  res.end(JSON.stringify(checkCascada(palabras)));
 });
 
-application.use(express.static('public'));
+application.use(express.static("public"));
 
+application.listen(8080, () => {
+  console.log("Servidor funcionando!");
+  fetchDict();
+});
 
-application.listen(8080, ()  => console.log('Servidor funcionando!'));
-
-
-
-
-
-
-function checkCascada(palabras) {
-    var res = []
-
-    //primero checkeo las definidas
-    checkPalabaraDefinida(palabras[0], "clan");
-    checkPalabaraDefinida(palabras[5], "pena");
-    checkPalabaraDefinida(palabras[6], "remato");
-    checkPalabaraDefinida(palabras[11], "torero");
-
-    let posPalabras = [1, 2, 3, 4, 7, 8, 9, 10];
-
-    posPalabras.forEach((p) => {
-        if (p % 2 == 0) checkPar(p);
-        else checkImpar(p);
-    });
-
-    function checkPalabaraDefinida(palabra, sol) {
-        if (palabra === sol) {
-            $(".palabra" + num).each(function (i) {
-                res.push(true)
-            });
-        }else{
-            res.push(false)
-        }
-    }
-
-    function checkPar(num) {
-        let palabra = palabras[num];
-        let palabraAnt = palabras[num - 1];
-
-        if (diccionario.includes(palabra) && parvalida(palabra, palabraAnt)) {
-            res.push(true)
-        } else {
-            res.push(false)
-        }
-    }
-
-    function checkImpar(num) {
-        let palabra = palabras[num];
-        let palabraAnt = palabras[num - 1];
-
-        if (diccionario.includes(palabra) && inparValida(palabra, palabraAnt)) {
-            res.push(true)
-        } else {
-            res.push(false)
-        }
-    }
-
-    function inparValida(str1, str2) {
-        if (str1.length != str2.length) return false;
-        let differences = 0;
-        for (let i = 0; i < str1.length; i++) {
-            if (str1.charAt(i) != str2.charAt(i))
-                if (++differences > 1) {
-                    return false;
-                }
-        }
-        if(differences == 0) return false;
-        return true;
-    }
-
-    function parvalida(str1, str2) {
-        return new Set(str1).size === new Set(str1 + str2).size && str1 != str2;
-    }
-    console.log(res)
-    return res
+var diccionario = [];
+//Descargar dict
+async function fetchDict() {
+  try {
+    await fetch("https://ordenalfabetix.unileon.es/aw/diccionario.txt")
+      .then((response) => response.text())
+      .then((response) => {
+        saveDict(response);
+      });
+  } catch (err) {
+    console.error("Error al cargar el diccionario. Intente de nuevo" + err);
+  }
 }
 
+//Guardar dict
+function saveDict(text) {
+  let dict = [];
+  text = text.split("\n");
+  text.forEach((element) => {
+    if (element.length == 4 || element.length == 6) {
+      dict.push(element);
+    }
+  });
+  //Anyado palabras necesarias para sol
+  dict.push("naci", "nací", "nace", "tolero");
+
+  diccionario = dict;
+}
+
+function checkCascada(palabras) {
+  var res = new Array(12);
+
+  //primero checkeo las definidas
+  checkPalabaraDefinida(0, "clan");
+  checkPalabaraDefinida(5, "pena");
+  checkPalabaraDefinida(6, "remato");
+  checkPalabaraDefinida(11, "torero");
+
+  let posPalabras = [1, 2, 3, 4, 7, 8, 9, 10];
+
+  posPalabras.forEach((p) => {
+    if (p % 2 == 0) checkPar(p);
+    else checkImpar(p);
+  });
+
+  function checkPalabaraDefinida(num, sol) {
+    if (palabras[num] === sol) {
+      res[num] = (true);
+    } else {
+      res[num] = (false);
+    }
+  }
+
+  function checkPar(num) {
+    let palabra = palabras[num];
+    let palabraAnt = palabras[num - 1];
+
+    if (diccionario.includes(palabra) && parvalida(palabra, palabraAnt)) {
+      res[num] = (true);
+    } else {
+      res[num] = (false);
+    }
+  }
+
+  function checkImpar(num) {
+    let palabra = palabras[num];
+    let palabraAnt = palabras[num - 1];
+
+    if (diccionario.includes(palabra) && inparValida(palabra, palabraAnt)) {
+      res[num] = (true);
+    } else {
+      res[num] = (false);
+    }
+  }
+
+  function inparValida(str1, str2) {
+    if (str1.length != str2.length) return false;
+    let differences = 0;
+    for (let i = 0; i < str1.length; i++) {
+      if (str1.charAt(i) != str2.charAt(i))
+        if (++differences > 1) {
+          return false;
+        }
+    }
+    if (differences == 0) return false;
+    return true;
+  }
+
+  function parvalida(str1, str2) {
+    return new Set(str1).size === new Set(str1 + str2).size && str1 != str2;
+  }
+  console.log(res);
+  return res;
+}
--- public/main.js
+++ public/main.js
@@ -4,184 +4,198 @@ var diccionario = [];
 var palabras = [];
 var pistas = 3;
 var id = 0;
-var localS = false;
+var localS;
 
 $(document).ready(function () {
-    if (window.localStorage.length != 0) localS = true;
-    if (!localS) localS = confirm("Permites el almacenamianto");
-    generatecascada();
-    if (window.localStorage.length != 0) loadStorage();
-    else initLocalStorage();
-    fetchDict();
+  localS = window.localStorage.getItem("localS");
+  if (!localS) localS = confirm("Permites el almacenamianto");
+  generatecascada();
+  if (localS && window.localStorage.length > 0) loadStorage();
+  else initLocalStorage();
+  fetchDict();
 
-    $("#restart").click(function (e) {
-        localStorage.clear();
-        location.reload();
-    });
+  $("#restart").click(function (e) {
+    localStorage.clear();
+    location.reload();
+  });
 
-    $("#comprobar").click(function (e) {
-        console.log(palabras.toString());
-        $.post("http://localhost:8080/palabras",{palabras: palabras.toString()}, function(data){
-            console.log(data);
-          });
-        });
-
-    $("#buscar").click(function (e) {
-        if (pistas > 0 && $("#letras").val() != "") {
-            pistas--;
-            if (localS) window.localStorage.setItem("pistas", pistas);
-            $("#pistasNum").text("Pistas: " + pistas);
-
-            let letras = $("#letras").val().split("");
-            let res = "";
-
-            diccionario.forEach((element) => {
-                if (letras.every((letra) => element.includes(letra))) {
-                    res += element + "\n";
-                }
+  $("#comprobar").click(function (e) {
+    $.post(
+      "http://localhost:8080/palabras",
+      { palabras: palabras.toString() },
+      function (data) {
+        data = data.replace("[", "");
+        data = data.replace("]", "");
+        let resultados = data.split(",");
+        for (let i = 0; i < resultados.length; i++) {
+          if (resultados[i] == 'true') {
+            $(".palabra" + i).each(function (i) {
+              $(this).addClass("bien");
             });
-            $("#pistas").val(res);
-        } else if ($("#letras").val() == "");
-        else $("#pistas").val("NO QUEDAN PISTAS");
-    });
-
-    $(".letra").on("keyup", function (e) {
-        readPalabras();
-        if (localS) window.localStorage.setItem("palabras", palabras);
-        //Moverse hacia delante al escribir
-        var index = $(this).index("input");
-        $(".letra:eq(" + (index + 1) + ")").focus();
-
-        //Moverse hacia detrás al borrar
-        if ((e.which == 8 || e.which == 46) && $(this).val() == "") {
-            $(".letra:eq(" + (index - 1) + ")").focus();
+          }
         }
-    });
-});
+      }
+    );
+  });
 
-//Descargar dict
-async function fetchDict() {
-    try {
-        await fetch("https://ordenalfabetix.unileon.es/aw/diccionario.txt")
-            .then((response) => response.text())
-            .then((response) => {
-                saveDict(response);
-            });
-    } catch (err) {
-        alert("Error al cargar el diccionario. Intente de nuevo");
+  $("#buscar").click(function (e) {
+    if (pistas > 0 && $("#letras").val() != "") {
+      pistas--;
+      if (localS) window.localStorage.setItem("pistas", pistas);
+      $("#pistasNum").text("Pistas: " + pistas);
+
+      let letras = $("#letras").val().split("");
+      let res = "";
+
+      diccionario.forEach((element) => {
+        if (letras.every((letra) => element.includes(letra))) {
+          res += element + "\n";
+        }
+      });
+      $("#pistas").val(res);
+    } else if ($("#letras").val() == "");
+    else $("#pistas").val("NO QUEDAN PISTAS");
+  });
+
+  $(".letra").on("keyup", function (e) {
+    readPalabras();
+    if (localS) window.localStorage.setItem("palabras", palabras);
+    //Moverse hacia delante al escribir
+    var index = $(this).index("input");
+    $(".letra:eq(" + (index + 1) + ")").focus();
+
+    //Moverse hacia detrás al borrar
+    if ((e.which == 8 || e.which == 46) && $(this).val() == "") {
+      $(".letra:eq(" + (index - 1) + ")").focus();
     }
-}
+  });
+});
 
 //Generar tablero
 function generatecascada() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
+  // Obtener la referencia del elemento body
+  var body = document.getElementsByTagName("body")[0];
 
-    cascada = document.getElementById("cascada");
+  cascada = document.getElementById("cascada");
 
-    // Crea un elemento <table> y un elemento <tbody>
-    tabla = document.createElement("table");
-    var tblBody = document.createElement("tbody");
+  // Crea un elemento <table> y un elemento <tbody>
+  tabla = document.createElement("table");
+  var tblBody = document.createElement("tbody");
 
-    // Crea las celdas
-    for (var i = 0; i <= 11; i++) {
-        // Crea las hileras de la tabla
-        var hilera = document.createElement("tr");
-        var len = 4;
+  // Crea las celdas
+  for (var i = 0; i <= 11; i++) {
+    // Crea las hileras de la tabla
+    var hilera = document.createElement("tr");
+    var len = 4;
 
-        if (i > 5) len = 6;
+    if (i > 5) len = 6;
 
-        for (var j = 0; j <= len; j++) {
-            var celda = document.createElement("td");
-            var input;
+    for (var j = 0; j <= len; j++) {
+      var celda = document.createElement("td");
+      var input;
 
-            if (j == 0) {
-                switch (i) {
-                    case 0:
-                        input = document.createTextNode("1️ ➡");
-                        break;
+      if (j == 0) {
+        switch (i) {
+          case 0:
+            input = document.createTextNode("1️ ➡");
+            break;
 
-                    case 5:
-                        input = document.createTextNode("2 ➡");
-                        break;
+          case 5:
+            input = document.createTextNode("2 ➡");
+            break;
 
-                    case 6:
-                        input = document.createTextNode("3 ➡");
-                        break;
+          case 6:
+            input = document.createTextNode("3 ➡");
+            break;
 
-                    case 11:
-                        input = document.createTextNode("4 ➡");
-                        break;
+          case 11:
+            input = document.createTextNode("4 ➡");
+            break;
 
-                    default:
-                        input = document.createTextNode("");
-                        break;
-                }
-            } else {
-                input = document.createElement("input");
-                input.setAttribute("type", "text");
-                input.setAttribute("maxLength", "1");
-                input.setAttribute("class", "letra palabra" + i);
-                input.setC;
-            }
-
-            celda.appendChild(input);
-            hilera.appendChild(celda);
+          default:
+            input = document.createTextNode("");
+            break;
         }
-        tblBody.appendChild(hilera);
+      } else {
+        input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("maxLength", "1");
+        input.setAttribute("class", "letra palabra" + i);
+        input.setC;
+      }
+
+      celda.appendChild(input);
+      hilera.appendChild(celda);
     }
-    tabla.appendChild(tblBody);
-    cascada.appendChild(tabla);
+    tblBody.appendChild(hilera);
+  }
+  tabla.appendChild(tblBody);
+  cascada.appendChild(tabla);
 }
 
 //Guardar dict
 function saveDict(text) {
-    let dict = [];
-    text = text.split("\n");
-    text.forEach((element) => {
-        if (element.length == 4 || element.length == 6) {
-            dict.push(element);
-        }
-    });
-    //Anyado palabras necesarias para sol
-    dict.push("naci", "nací", "nace", "tolero");
+  let dict = [];
+  text = text.split("\n");
+  text.forEach((element) => {
+    if (element.length == 4 || element.length == 6) {
+      dict.push(element);
+    }
+  });
+  //Anyado palabras necesarias para sol
+  dict.push("naci", "nací", "nace", "tolero");
 
-    diccionario = dict;
+  diccionario = dict;
+}
+
+//Descargar dict
+async function fetchDict() {
+  try {
+    await fetch("https://ordenalfabetix.unileon.es/aw/diccionario.txt")
+      .then((response) => response.text())
+      .then((response) => {
+        saveDict(response);
+      });
+  } catch (err) {
+    alert("Error al cargar el diccionario. Intente de nuevo");
+  }
 }
 
 function readPalabras() {
-    palabras = [];
-    for (let i = 0; i < 12; i++) {
-        let palabra = "";
-        $(".palabra" + i).each(function (i) {
-            palabra += $(this).val();
-            if ($(this).val() === "") palabra += "";
-        });
+  palabras = [];
+  for (let i = 0; i < 12; i++) {
+    let palabra = "";
+    $(".palabra" + i).each(function (i) {
+      palabra += $(this).val();
+      if ($(this).val() === "") palabra += " ";
+    });
 
-        palabras.push(palabra);
-    }
+    palabras.push(palabra);
+  }
 }
 
 function loadStorage() {
-    localS = window.localStorage.getItem("localS");
-    palabras = window.localStorage.getItem("palabras").split(",");
-    pistas = window.localStorage.getItem("pistas");
-    $("#pistasNum").text("Pistas: " + pistas);
-    let casilla = [];
+  localS = window.localStorage.getItem("localS");
+  palabras = window.localStorage.getItem("palabras").split(",");
+  pistas = window.localStorage.getItem("pistas");
+  $("#pistasNum").text("Pistas: " + pistas);
+  let casilla = [];
 
-    palabras.forEach((element) => {
-        let chars = element.split("");
-        chars.forEach((e) => {
-            casilla.push(e);
-        });
+  palabras.forEach((element) => {
+    let chars = element.split("");
+    chars.forEach((e) => {
+      casilla.push(e);
     });
+  });
 
-    $(".letra").each(function (i) {
-        $(this).val(casilla.shift(0));
-    });
+  $(".letra").each(function (i) {
+    if (casilla[0] != " ") $(this).val(casilla.shift(0));
+    else casilla.shift(0);
+  });
 }
 
 function initLocalStorage() {
-    window.localStorage.setItem('pistas',3)
-    window.localStorage.setItem('palabras',palabras);
+  window.localStorage.setItem("localS", true);
+  window.localStorage.setItem("pistas", 3);
+  window.localStorage.setItem("palabras", palabras);
 }
